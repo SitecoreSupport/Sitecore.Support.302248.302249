397a5ec82b12672c64e8aeeeb3232afa43bd61bb
dk\ivd <ivd@sitecore.net>
2019-01-08 15:36:21 +0800

    Inject related types

---
+++ src/Sitecore.Support.302248.302249/RefreshTree.cs
@@ -0,0 +1,117 @@
+﻿
+using Sitecore.Abstractions;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Maintenance;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Jobs;
+using Sitecore.Shell.Applications.Dialogs.ProgressBoxes;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Web.UI.Sheer;
+using System;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading;
+using CSTexts = Sitecore.ContentSearch.Localization.Texts;
+
+namespace Sitecore.Support.ContentSearch.Client.Commands
+{
+
+  [Serializable]
+  public class RefreshTree : Sitecore.ContentSearch.Client.Commands.RefreshTree
+  {
+    public override void Execute([NotNull] CommandContext context)
+    {
+      Assert.ArgumentNotNull(context, "context");
+
+      var item = context.Items[0];
+      Assert.IsNotNull(item, "context item cannot be null");
+
+      Context.ClientPage.Start(this, "RunFixed", new NameValueCollection { { "itemUri", item.Uri.ToString() }, { "itemPath", item.Paths.ContentPath } });
+    }
+
+    protected void RunFixed(ClientPipelineArgs args)
+    {
+      string itemPath = args.Parameters["itemPath"];
+      if (string.IsNullOrEmpty(itemPath))
+      {
+        return;
+      }
+
+      var jobName = string.Format("{0} ({1})", this.Translate.Text(CSTexts.ReIndexTree), itemPath);
+      var headerText = this.Translate.Text(CSTexts.ReIndexTreeHeader);
+      ProgressBox.ExecuteSync(jobName, headerText, "Applications/16x16/replace2.png", this.Refresh, this.RefreshDone);
+    }
+
+    private void RefreshDone(ClientPipelineArgs args)
+    {
+      var message = args.Parameters["failed"] == "1" ?
+                   CSTexts.ReindexTreeFailed :
+                   CSTexts.ReIndexTreeComplete;
+
+      var translatedMessage = this.Translate.Text(message);
+
+      SheerResponse.Alert(translatedMessage);
+    }
+
+    private void Refresh(ClientPipelineArgs args)
+    {
+      this.JobHandle = Context.Job.Handle;
+
+      Item item = GetItemByUri(args.Parameters["itemUri"]);
+
+      if (item == null)
+      {
+        return;
+      }
+
+      this.Event.Subscribe("indexing:updateditem", this.ShowProgress);
+
+      Log.Audit($"Refresh indexes for item: {AuditFormatter.FormatItem(item)}", this);
+      var jobs = IndexCustodian.RefreshTree((SitecoreIndexableItem)item).ToList();
+
+      while (jobs.Any(j => !j.IsDone))
+      {
+        Thread.Sleep(500);
+      }
+
+      this.Event.Unsubscribe("indexing:updateitem", this.ShowProgress);
+
+      Job failedJob = jobs.FirstOrDefault(j => j.Status.Failed);
+      if (failedJob != null)
+      {
+        args.Parameters["failed"] = "1";
+      }
+    }
+
+    private void ShowProgress(object sender, EventArgs eventArgs)
+    {
+      object[] parameters = this.Event.ExtractParameters(eventArgs);
+
+      if (parameters == null || parameters.Length < 3)
+      {
+        return;
+      }
+
+      var itemPath = parameters[2] as string;
+
+      if (!string.IsNullOrEmpty(itemPath) && this.JobHandle != null)
+      {
+        var job = JobManager.GetJob(this.JobHandle);
+        if (job != null)
+        {
+          job.Status.Messages.Add(itemPath);
+        }
+      }
+    }
+
+    private static Item GetItemByUri(string uri)
+    {
+      var itemUri = ItemUri.Parse(uri);
+      Database db = ContentSearchManager.Locator.GetInstance<IFactory>().GetDatabase(itemUri.DatabaseName);
+      Item item = db.GetItem(itemUri.ToDataUri());
+      return item;
+    }
+  }
+}
